56744fb664f9b6d39264d9dc060c08ae06381e15
liuyi <liuyicn6@qq.com>
2023-06-19 22:33:48 +0800

    finish lab1

--- lab01/ex10_ll_cycle.c
+++ lab01/ex10_ll_cycle.c
@@ -2,5 +2,21 @@
 #include "ex10_ll_cycle.h"
 
 int ll_has_cycle(node *head) {
+    if (head == NULL || head->next == NULL) {
+        return 0;
+    } 
+    node* slow_node = head;
+    node* fast_node = head;
+    while (1) {
+        if (fast_node->next == NULL || fast_node->next->next == NULL) {
+            return 0;
+        }
+        fast_node = fast_node->next->next;
+        slow_node = slow_node->next;
+        if (slow_node == fast_node) {
+            return 1;
+        }
+    }
+
     /* TODO: Implement ll_has_cycle */
 }
