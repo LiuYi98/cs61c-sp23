0a003d4dd271fa974db16cd178cfec58ca6e7665
liuyi <liuyicn6@qq.com>
2023-06-25 12:18:35 +0800

    finish lab2

--- lab02/ex5_valgrind.c
+++ lab02/ex5_valgrind.c
@@ -3,14 +3,14 @@
 #include <string.h>
 
 char *copy_str(char *s) {
-    char *new_str = malloc(strlen(s) * sizeof(char));
+    char *new_str = malloc((strlen(s) + 1) * sizeof(char));
     strcpy(new_str, s);
     return new_str;
 }
 
 int main() {
-    char * copy_of_str = copy_str("hello world!");
+    char *copy_of_str = copy_str("hello world!");
     printf("%s\n", copy_of_str);
-
+    free(copy_of_str);
     return 0;
 }
